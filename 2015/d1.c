c6d740ed8082fe47728164496a4780ac07944191
kenuy <guilhermesaavedranunes@gmail.com>
2024-12-01 01:29:18 -0300

    edit: problema 2015/1d

--- 2015/d1.c
+++ 2015/d1.c
@@ -1,3 +1,18 @@
+/*
+    --- Day 1: Not Quite Lisp ---
+    Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
+
+    - An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+    - The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
+
+    For example:
+    (()) and ()() both result in floor 0.
+    ((( and (()(()( both result in floor 3.
+    ))((((( also results in floor 3.
+    ()) and ))( both result in floor -1 (the first basement level).
+    ))) and )())()) both result in floor -3.
+    To what floor do the instructions take Santa?
+*/
 #include<stdio.h>
 #include<string.h>
 
@@ -16,7 +31,18 @@ int main() {
         }
     }
 
-    printf("O papai noel parou no andar %d\n", andar);
+    printf("O papai noel parou no andar %d\n", andar); // 1.1 saída = 138
+    //  The first half of this puzzle is complete! It provides one gold star: *
+
+    /*
+        --- Part Two ---
+        Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+
+        For example:
+        ) causes him to enter the basement at character position 1.
+        ()()) causes him to enter the basement at character position 5.
+        What is the position of the character that causes Santa to first enter the basement?
+    */
     printf("O papai noel entrou no porao pela primeira vez no caracter na posicao %d\n", entrou_porao);
 
     return 0;
